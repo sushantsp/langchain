d43c0d949194aa5a6aa218068a15d3c6ee707050
Sushant <32874962+sushantsp@users.noreply.github.com>
2023-11-01 23:10:40 +0530

    Add files via upload

---
+++ pet_names/langchain_helper.py
@@ -0,0 +1,23 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_pet_names(animal_type, pet_color):
+    llm = OpenAI(temperature = 0.7)
+
+    prompt_template = PromptTemplate(
+        input_variables = ['animal_type', 'pet_color'],
+        template = "I have a {animal_type} pet and I want cool names for it, it is {pet_color} in color. Suggest me five cool names for my pet."
+    )
+
+    name_chain = LLMChain( llm = llm, prompt = prompt_template, output_key="pet_names")
+
+    response = name_chain({"animal_type": animal_type, "pet_color": pet_color})
+    return response["pet_names"]
+
+
+if __name__ == "__main__":
+    print(generate_pet_names("cow", "black"))
